bdc3e6f89ef9efae0619bbd04cc51488cf575b23
Chakib Cerny <ccerny@rollins.edu>
2021-04-30 14:12:53 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import random
+
+import librosa
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torchaudio
 import torchvision
 import torchvision.datasets as datasets
+from scipy.io import wavfile
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter  # to print to tensorboard
@@ -15,6 +18,8 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import soundfile as sf
+import noisereduce as nr
+from scipy.io.wavfile import write
 
 #######tensorboard --logdir C:\Users\chaki\PycharmProjects\GAN\logs
 #######http://localhost:6006/
@@ -23,25 +28,26 @@ class Discriminator(nn.Module):
     def __init__(self, in_features):
         super().__init__()
         self.disc = nn.Sequential(
-            nn.Linear(in_features, 500),
+            nn.Linear(in_features, 128),
             nn.LeakyReLU(0.01),
-            nn.Linear(500, 1),
+            nn.Linear(128, 1),
             nn.Sigmoid(),
         )
 
     def forward(self, x):
         return self.disc(x)
 
+#use mgan
 
 class Generator(nn.Module):
     def __init__(self, z_dim, img_dim):
         super().__init__()
         self.gen = nn.Sequential(
-            nn.Linear(z_dim, 2000),
+            nn.Linear(z_dim, 512),
             nn.LeakyReLU(0.01),
-            nn.Linear(2000, img_dim),
+            nn.Linear(512, img_dim),
             nn.LeakyReLU(0.01),
-            #nn.Sigmoid(),
+            #nn.tanh(),
         )
 
     def forward(self, x):
@@ -49,8 +55,8 @@ class Generator(nn.Module):
 
 
 # Hyperparameters etc.
-#device = "cuda" if torch.cuda.is_available() else "cpu"
-device = "cpu"
+device = "cuda" if torch.cuda.is_available() else "cpu"
+#device = "cpu"
 lr = 3e-4
 z_dim = 64
 image_dim = 250000 * 1
@@ -79,9 +85,8 @@ for row in range(300):
     waveform_mono = torch.mean(waveform, dim=0, keepdim=True)
     #print(waveform_mono.size())
     waveform_mono = torch.narrow(waveform_mono, 1, 0, 250000)
-    ps = waveform_mono
     #print(ps.size())
-    D.append((ps, str(row)))
+    D.append((waveform_mono, str(row)))
 
 dataset = D
 random.shuffle(dataset)
@@ -96,6 +101,10 @@ criterion = nn.BCELoss()
 writer_fake = SummaryWriter(f"logs/fake")
 writer_real = SummaryWriter(f"logs/real")
 step = 0
+
+toplotG = []
+toplotD = []
+ep = list(range(1, num_epochs+1))
 
 for epoch in range(num_epochs):
     for batch_idx, (real) in enumerate(loader):
@@ -129,11 +138,17 @@ for epoch in range(num_epochs):
                 f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
                       Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
             )
+            toplotG.append((lossG))
+            toplotD.append((lossD))
 
             with torch.no_grad():
                 fake = gen(fixed_noise)
-                #fake = torch.narrow(fake, 0, 0, 2)
-                torchaudio.save(str(epoch)+'.wav', fake, fixed_sample_rate)
+                # perform noise reduction
+                #torchaudio.save(str(epoch)+'.wav', fake.cpu(), fixed_sample_rate)
+                #data, rate = librosa.load(str(epoch)+'.wav')
+                #noisy_part = data[0:250000]
+                #reduced_noise = nr.reduce_noise(audio_clip=data, noise_clip=noisy_part, verbose=True)
+                #write(str(epoch)+'rn.wav', rate, reduced_noise)
                 #fake = fake[1]
                 #print(fake.size())
                 #output_signal = tf.audio.encode_wav(fake, fixed_sample_rate)
@@ -160,8 +175,14 @@ for epoch in range(num_epochs):
                 #)
                 step += 1
 
-    #librosa.display.specshow(ps2, y_axis='mel', x_axis='time')
-    #plt.show()
-    #librosa.output.write_wav('/generated/test', ps2[0], ps2[1], norm=False)
+plt.plot(ep, toplotG)
+plt.xlabel('Epoch')
+plt.ylabel('Generator loss')
+plt.show()
+
+plt.plot(ep, toplotD)
+plt.xlabel('Epoch')
+plt.ylabel('Discriminator loss')
+plt.show()
 
 ########output_signal = tf.audio.encode_wav(input_signal[0], input_signal[1])
